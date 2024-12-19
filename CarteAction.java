689c559c1652919d83602cd2634a234c60c4c483
sarabenbraham <155384655+sarabenbraham@users.noreply.github.com>
2024-12-19 16:36:08 +0100

    modificationclassescartes

--- CarteAction.java
+++ CarteAction.java
@@ -1,7 +1,7 @@
 public class CarteAction extends Carte{
     public CarteSpeciale(char couleur, String action) {
         super(couleur, action);
-        if (!action.equals("+2") && !action.equals("Inverser") && !action.equals("Passer")) {
+        if (!action.equals("+2") && !action.equals("Inverser") && !action.equals("bloquer")) {
             throw new IllegalArgumentException("Action non valide");
         }
     }    
--- CarteNormale.java
+++ CarteNormale.java
@@ -1,5 +1,6 @@
 public class CarteNormale extends Carte{
     public CarteNormale(String.valeurof(int),couleur){
+        super(couleur, String.valueOf(numero));
         if (numero < 0 || numero > 9) {
             throw new IllegalArgumentException("donner un num entre 0 et 9");
         }
